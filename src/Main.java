01397f09fbc6a16352584958bf87f88395c2f829
”Camopogok” <zagrebin.dm@gmail.com>
2023-01-29 16:11:08 +0300

    Добавил активность полей и проверку на пустое своё значение

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static javax.swing.SwingConstants.CENTER;
+
 public class Main {
     JFrame frame = new JFrame("Цвета");
-    JPanel jpanel = new JPanel();
     JPanel panel = new JPanel();
-    JPanel panel1 = new JPanel();
-    JPanel panel2 = new JPanel();
-    JPanel panel3 = new JPanel();
-    JPanel panel4 = new JPanel();
     String [] colors = {"Красный", "Синий", "Зелёный", "Жёлтый", "Белый", "Чёрный"};
-    JComboBox comboBox = new JComboBox(colors);
+    JComboBox<String> comboBox = new JComboBox<>(colors);
     JCheckBox checkBox = new JCheckBox("Свой вариант");
     JTextField textField = new JTextField(8);
     JButton button = new JButton("Ответить");
@@ -18,34 +15,57 @@ public class Main {
 
 
     public Main () {
-        frame.setSize(140,250);
-        frame.setLocation(600,400);
+        int width = 140, height = 250;
+        frame.setSize(width,height);
+        Dimension monitorResolution = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(monitorResolution.width/2 - width/2,monitorResolution.height/2 - height/2);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        panel.add(comboBox);
-        panel1.add(checkBox);
-        panel2.add(textField);
-        panel3.add(button);
-        panel4.add(label);
+
         button.addActionListener(actionEvent -> {
             if (checkBox.isSelected()) {
-                label.setText("Ответ: " + textField.getText());
+                if (!textField.getText().equals(""))
+                    label.setText("Ответ: " + textField.getText());
+                else {
+                    label.setFont(new Font("Dialog", Font.BOLD, 12));
+                    label.setHorizontalAlignment(CENTER);
+                    label.setText("<HTML> <Font color='red'> Введите цвет </Font> </html>");
+                }
             } else {
-                label.setText("Ответ: " + (String)comboBox.getSelectedItem());
+                label.setText("Ответ: " + comboBox.getSelectedItem());
             }
             });
-        jpanel.setLayout(new GridLayout(5, 1));
-        panel.setLayout(new FlowLayout());
-        panel1.setLayout(new FlowLayout());
-        panel2.setLayout(new FlowLayout());
-        panel3.setLayout(new FlowLayout());
-        panel4.setLayout(new FlowLayout());
-        jpanel.add(panel);
-        jpanel.add(panel1);
-        jpanel.add(panel2);
-        jpanel.add(panel3);
-        jpanel.add(panel4);
-        frame.add(jpanel);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(Box.createVerticalStrut(10));
+
+        comboBox.setMaximumSize(new Dimension(100,25));
+
+        textField.setMaximumSize(new Dimension(100,30));
+        textField.setEnabled(false);
+
+        checkBox.addActionListener(actionEvent -> {
+            if (checkBox.isSelected()) {
+                textField.setEnabled(true);
+                comboBox.setEnabled(false);
+            } else {
+                textField.setEnabled(false);
+                comboBox.setEnabled(true);
+            }
+        });
+
+        addComponent(comboBox);
+        addComponent(checkBox);
+        addComponent(textField);
+        addComponent(button);
+        addComponent(label);
+        frame.add(panel);
         frame.setVisible(true);
+    }
+
+    public void addComponent (JComponent c) {
+        c.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(c);
+        panel.add(Box.createVerticalStrut(10));
     }
 
     public static void main(String[] args) {
